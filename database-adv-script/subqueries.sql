de9eb2828fb4cbad4fbc38b026f74358e094eb63
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-07 18:13:51 +0200

    for Average rating correct

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,4 +1,11 @@
-SELECT * FROM Property WHERE property_id IN (SELECT property_id  FROM Review WHERE rating > 4);
+SELECT *
+FROM Property
+WHERE id IN (
+    SELECT property_id
+    FROM Review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
 
 SELECT u.user_id, u.first_name, u.last_name, u.email
 FROM User u
