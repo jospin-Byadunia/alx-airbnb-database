7a08dc54d6d6bdad004185df69eb497ae885e276
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-07 18:21:02 +0200

    Aggregations and Window Functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,14 @@
+SELECT *, COUNT(Booking.booking_id) AS total_bookings 
+FROM User LEFT JOIN Booking ON User.user_id=Booking.user_id 
+GROUP BY User.user_id ORDER BY total_bookings DESC;
+
+
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM Property p
+LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name
+ORDER BY booking_rank;
