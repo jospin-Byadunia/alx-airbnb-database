26ffb3e4f94f05187ea7a9fc66830624153e58a4
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-07 18:01:44 +0200

    Add retrieving order

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,5 +1,5 @@
 SELECT * FROM Booking INNER JOIN User ON Booking.user_id=User.user_id 
 
-SELECT * FROM Booking LEFT JOIN User ON Booking.user_id=User.user_id
+SELECT * FROM Booking LEFT JOIN User ON Booking.user_id=User.user_id ORDER BY Booking.booking_date DESC
 
 SELECT * FROM Booking FULL OUTER JOIN User ON Booking.user_id=User.user_id
